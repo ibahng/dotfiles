549b7bb9f9f272850737d638211a4f6c196d6468
ibahng <ibahng21@gmail.com>
2026-03-02 15:38:25 +0900

    added some hammerspoon shortkeys

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -13,9 +13,20 @@ spoon = spoon
 
 
 hs.hotkey.bind({"alt"}, "`", function()
-  hs.application.launchOrFocus("Calendar")
+  hs.application.launchOrFocus("ChatGPT")
 end)
 
+hs.hotkey.bind({"alt"}, "B", function()
+  hs.application.launchOrFocus("Brave Browser")
+end)
+
+hs.hotkey.bind({"alt"}, "N", function()
+  hs.application.launchOrFocus("Notes")
+end)
+
+hs.hotkey.bind({"alt"}, "\\", function()
+  hs.application.launchOrFocus("kitty")
+end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
